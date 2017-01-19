d1cdfb2718e3c4c9c5b8078c4467975c65a8a1d9
Benjamin Willms <benjamin.willms@mail.mcgill.ca>
2017-01-19 17:59:11 -0500

    Initial repo creation
    Created the repo for collaborative work during the assignment

---
+++ src/ca/mcgill/ecse321/eventregistration/controller/EventRegistrationController.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.eventregistration.controller;
+
+import ca.mcgill.ecse321.eventregistration.model.RegistrationManager;
+
+public class EventRegistrationController {
+	
+	private RegistrationManager rm;
+
+	public EventRegistrationController(RegistrationManager rm) {
+		this.rm = rm;
+	}
+
+	public void createParticipant(String name) {
+		
+	}
+}
